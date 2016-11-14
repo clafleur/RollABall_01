1412faf7f5760ddb754fbc85be67b33d2852e5e3
clafleur <clayton@texsstudio.com>
2016-11-14 10:55:48 -0800

    Initial Checkin
    Updated the .gitignor for the initial check in

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+    public float speed = 10;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertial = Input.GetAxis("Vertical");
+
+        //get the movement of the player
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertial);
+
+        //add a force to the rigidbody
+        rb.AddForce(movement * speed);
+    }
+}
