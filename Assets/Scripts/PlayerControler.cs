36329cd1914d849ee8ff0990ee5beb0c2d19ca99
clafleur <clayton@texsstudio.com>
2016-11-15 08:13:12 -0800

    Finished the tutorial
    Added counting and count tracking to the player script.
    Added collectables and made them disappear when collected.
    Changed the collectibles to have a rigidbody and be kinematic to allow
    the player to collect them

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerControler : MonoBehaviour {
     public float speed = 10;
-    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
 
+    private Rigidbody rb;
+    private int count;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate ()
     {
@@ -19,5 +26,24 @@ public class PlayerControler : MonoBehaviour {
 
         //add a force to the rigidbody
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 16)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
